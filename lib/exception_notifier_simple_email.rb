9bcb8eeebcb820b7e1284cc6040947423f6a99b0
Artur Hebda <arturhebda@gmail.com>
2015-01-11 12:09:15 +0100

    Generate gem skeleton with bundle gem exception_notifier_simple_email

---
+++ lib/exception_notifier_simple_email.rb
@@ -0,0 +1,5 @@
+require 'exception_notifier_simple_email/version'
+
+module ExceptionNotifierSimpleEmail
+  # Your code goes here...
+end
---
+++ lib/exception_notifier_simple_email/version.rb
@@ -0,0 +1,3 @@
+module ExceptionNotifierSimpleEmail
+  VERSION = "0.0.1"
+end
